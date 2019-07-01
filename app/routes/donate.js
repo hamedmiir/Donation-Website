8bf0fc6938a56060d80adbb4610dab87f2dd7098
hamedmiir <mirzaeihamed@yahoo.com>
2019-07-01 15:23:32 +0430

    Some changes in views

--- app/routes/donate.js
+++ app/routes/donate.js
@@ -3,7 +3,6 @@ const config = require('config')
 module.exports = router => {
   router.get("/donate", async ctx => {
     ctx.state.model = {
-      title: "Donate",
       amount: ctx.query.amount || config.get("settings.defaultDonation")
     }
     await ctx.render('donate')
