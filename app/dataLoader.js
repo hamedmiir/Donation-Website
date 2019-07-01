d9567c0ed158a50ed8a08fc59c262c6c724c57f1
hamedmiir <mirzaeihamed@yahoo.com>
2019-07-01 13:04:18 +0430

    basic files and views added

---
+++ app/dataLoader.js
@@ -0,0 +1,36 @@
+cosnt path = require('path')
+const fs = require('fs-extra')
+
+function fileInfo(fileName, dir) {
+    return {
+        slug: fileName.substr(0, fileName.indexOf('.json')),
+        name: fileName,
+        path: path.join(dir, fileName) 
+    }
+}
+
+function readFile(fileInfo) {
+    return fs
+        .readJson(fileInfo.path)
+        .then(content => Object.assign(content,
+            { _slug: fileInfo.slug }))
+}
+
+class DataLoader {
+    constructor(dir) {
+        this.dir = dir;
+    }
+
+    async all() {
+        const fileInfos = (await fs.readdir(this.dir)).map(fileName => fileInfo(fileName, this.dir))
+        return Promise.all(fileInfos.map(readFile))
+    }
+
+    async single(slug) {
+        const fileInfos = (await fs.readdir(this.dir)).map(fileName => fileInfo(fileName, this.dir))
+        var found = fileInfos.find(file => file.slug === slud)
+        return found ? readFile(found) : null
+    }
+}
+
+module.export = DataLoader
---
+++ index.js
@@ -0,0 +1,45 @@
+const config = require('config')
+const path = require('path')
+const Koa = require('koa')
+const Router = require('koa-router')
+const loadRoutes = require("./app/routes")
+const DataLoader = require("./app/dataLoader")
+const views = require('koa-views')
+const serve = require(koa-static)
+
+const app = new Koa()
+const router = new Router()
+
+// Reads json files
+const productsLoader = new DataLoader(
+    path.join(
+        __dirname,
+        config.get('data.path'),
+        'products')
+)
+
+// Views setup
+app.use(views(
+    path.join(__dirname, config.get('views.path')),
+    config.get('views.options')
+))
+
+// Static files
+app.use(serve(config.get('static.path')))
+
+// Hydrate ctx.state with global settings
+app.use(async (ctx, next) => {
+    ctx.state.settings = config.get('settings')
+    ctx.state.urlWithoutQuery = ctx.origin + ctx.path
+    await next()    //pass to next middleware
+})
+
+// Config router
+loadRoutes(router, productsLoader)
+app.use(router.routes())
+
+// Start
+const port = process.env.PORT || config.get('server.port')
+app.listen(port, () => {
+    console.log('Application is listening on port ${port}')
+})
